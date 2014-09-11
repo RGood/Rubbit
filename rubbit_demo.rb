593f9f13a98d356d1ce2def4508c9384a6a52060
Goodman, Randy <rgoodman@constantcontact.com>
2014-09-11 11:52:39 -0400

    Added some comments to the demo program

--- rubbit_demo.rb
+++ rubbit_demo.rb
@@ -1,11 +1,15 @@
 require 'Rubbit'
 
+#Initialize Rubbit client
 r = Rubbit.new 'Demo Program'
 
+#Create an object that represents /r/AskReddit
 AskReddit = r.get_subreddit('AskReddit')
 
+#Create an enumurable object that represents the top 5 posts on the /r/AskReddit frontpage
 hot = AskReddit.get_hot(5)
 
+#Iterate through each and output the title
 hot.each do |post|
 	puts post.title
 end
