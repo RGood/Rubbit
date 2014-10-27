9f732f8ed87ba0035668b8b37b21175ad01caccf
Goodman, Randy <rgoodman@constantcontact.com>
2014-10-27 11:31:05 -0400

    Update demo

--- rubbit_demo.rb
+++ rubbit_demo.rb
@@ -9,7 +9,7 @@ AskReddit = r.get_subreddit('AskReddit')
 #Create an enumurable object that represents the top 5 posts on the /r/AskReddit frontpage
 hot = AskReddit.get_hot(5)
 
-#Iterate through each and output the title
+#Iterate through each post and output the title
 hot.each do |post|
 	puts post.title
 end
